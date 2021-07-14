195574dd208aa3a0a5d9837463ca3ba932a3cf82
Danish <danish@ultroid.tech>
2021-07-14 21:13:05 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 echo "Cloning the repo..."
 # clone the repo
-git clone https://github.com/TeamUltroid/Ultroid.git /root/TeamUltroid
+git clone https://github.com/TeamUltroid/Ultroid.git /root/TeamUltroid/
 
 echo "Cloned Successfully."
